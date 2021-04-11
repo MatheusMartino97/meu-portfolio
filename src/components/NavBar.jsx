eb267232019a4b5c3986e0032d3fc57db0f878e1
Matheus Martino <matheus.martino97@gmail.com>
2021-04-11 05:25:37 -0300

    Lint issues.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,10 +9,10 @@ class NavBar extends Component {
           <h1>Matheus Martino do Amaral</h1>
         </div>
       <div className="nav-social-links">
-        <a className="social-link" href="https://github.com/MatheusMartino97/" target="_blank">
+        <a className="social-link" href="https://github.com/MatheusMartino97/" target="_blank" rel="noreferrer">
           <img src="https://cdn.iconscout.com/icon/free/png-256/github-108-438008.png" width="48px" height="48px" alt="Github"/>
         </a> 
-        <a className="social-link" href="https://www.linkedin.com/in/matheus-martino/" target="_blank">
+        <a className="social-link" href="https://www.linkedin.com/in/matheus-martino/" target="_blank" rel="noreferrer">
           <img src="https://i.ibb.co/Kx2GSrT/linkedin.png" width="48px" height="48px" alt="Linked In"/>
         </a>
       </div>
