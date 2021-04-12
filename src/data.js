4d19ab2bb27c5385b8372b32e7365df11e80ec84
Matheus Martino <matheus.martino97@gmail.com>
2021-04-12 20:05:05 -0300

    Adicionando projetos.

--- src/data.js
+++ src/data.js
@@ -2,64 +2,93 @@ const projectsList = {
   toDoList: {
     name: 'To Do List',
     translation: 'Lista de Tarefas',
+    path: 'to-do-list',
     miniature: 'https://i.ibb.co/cbvzQ6M/to-do-list-miniature.jpg',
     url: 'https://matheusmartino97.github.io/04_sd-09-project-todo-list/',
-    path: 'to-do-list',
-    repository: "https://github.com/MatheusMartino97/04_sd-09-project-todo-list" ,
+    repository:
+      'https://github.com/MatheusMartino97/04_sd-09-project-todo-list',
     gifs: {
-      mobile: "https://i.ibb.co/8PBGwpm/to-do-list-gif-mobile.gif",
-      desktop: "https://i.ibb.co/28dvCTY/to-do-list-gif-desktop.gif",
+      mobile: 'https://i.ibb.co/8PBGwpm/to-do-list-gif-mobile.gif',
+      desktop: 'https://i.ibb.co/28dvCTY/to-do-list-gif-desktop.gif',
     },
     descriptions: {
       short: 'Lista de tarefa simples estilizada com Bulma.',
       long: {
-        paragraph1: 'Este aqui foi uns dos primeiros projetos que desenvolvi na Trybe. Esta lista de tarefas, que, apesar de ser relativamente simples, me fez perceber que desenvolvimento de softwares seria, definitivamente, a minha profissão',
-        paragraph2: 'Durante o desenvolvimento deste projeto, percebi também a importância da metodologia Mobile First. Entendi que pode ser extremamente vantajoso produzir primeiro para telas pequenas e, posteriormente, expandir para telas maiores.',
-        paragraph3: 'Além disso, foi aqui que tive o meu primeiro contato com qualquer tipo de biblioteca externa. Utilizei Bulma para estilizar este aplicativo.'
+        paragraph1:
+          'Este aqui foi uns dos primeiros projetos que desenvolvi na Trybe. Esta lista de tarefas, que, apesar de ser relativamente simples, me fez perceber que desenvolvimento de softwares seria, definitivamente, a minha profissão',
+        paragraph2:
+          'Durante o desenvolvimento deste projeto, percebi também a importância da metodologia Mobile First. Entendi que pode ser extremamente vantajoso produzir primeiro para telas pequenas e, posteriormente, expandir para telas maiores.',
+        paragraph3:
+          'Além disso, foi aqui que tive o meu primeiro contato com qualquer tipo de biblioteca externa. Utilizei Bulma para estilizar este aplicativo.',
       },
-    }
+    },
   },
 
   pixelsArt: {
     name: 'Pixels Art',
     translation: 'Arte com Pixels',
+    path: 'pixels-art',
     miniature: 'https://i.ibb.co/5r6wCCZ/pixel-art-miniature.jpg',
     url: 'https://matheusmartino97.github.io/04_sd-09-project-todo-list/',
-    path: 'pixels-art',
+    repository:
+      'https://github.com/MatheusMartino97/03_sd-09-project-pixels-art',
     gifs: {
-      mobile: "",
-      desktop: "",
+      mobile: '',
+      desktop: 'https://i.ibb.co/jgPXJ6g/pixels-art-desktop.gif',
     },
     descriptions: {
-      short: "Um canvas simples feito com JavaScript puro.",
-      long: ""
-    }
+      short: 'Um canvas simples feito com JavaScript puro.',
+      long: {
+        paragraph1:
+          'Um projeto bem divertido feito com HTML, CSS e JavaScript puro.',
+        paragraph2:
+          'Pra mim, a parte mais interessante de desenvolvê-lo, como iniciante em programação, foi treinar iterações e me familiarizar como a lógica de programação é aplicada na pratica.',
+        paragraph3:
+          'Essa é a primeira página que um aluno desenvolve utilizando JavaScript na formação da Trybe e, acredito que, tenha sido importantíssimo para nos dar motivação e nos apresentar a imensidão de posibilidades que um programador pode desenvolver.',
+      },
+    },
   },
 
   shoppingCard: {
     name: 'Shopping Card',
     translation: 'Carrinho de Compras',
+    path: 'shopping-cart',
     miniature: 'https://i.ibb.co/n35vBC6/shopping-cart-miniature.jpg',
     url: 'https://matheusmartino97.github.io/12_sd-09-project-shopping-cart/',
-    path: 'shopping-cart',
+    repository:
+      'https://github.com/MatheusMartino97/12_sd-09-project-shopping-cart',
     gifs: {
-      mobile: "",
-      desktop: "",
+      mobile: '',
+      desktop: 'https://i.ibb.co/Cm0W0Lh/ezgif-com-gif-maker.gif',
     },
     descriptions: {
-      short: "Um carrinho de compras básico utilizando a API do Mercado Livre Brasil.",
-      long: ""
+      short:
+        'Um carrinho de compras básico utilizando a API do Mercado Livre Brasil.',
+      long: {
+        paragraph1: 'O meu primeiro contato com consumo de APIs foi desenvolvendo este projeto. Aprendi como fazer requisições para uma API, como interpretar sua documentação e como tratar sua resposta para que esta esteja no formato desejado.',
+        paragraph2: 'Além disso, pratiquei a manipular e utilizar o Local Storage para guardar informações necessárias dos usuários da aplicação.',
+      },
     },
   },
 
   // projectName: {
   //   name: "",
-  //   description: "",
   //   translation: "",
+  //   path: "",
   //   miniature: "",
   //   url: "",
-  //   path: "",
-  // }
+  //   repository: '',
+  //   gifs: {
+  //     mobile: '',
+  //     desktop: '',
+  //   },
+  //   descriptions: {
+  //     short: '',
+  //     long: {
+  //       paragraph1: ''
+  //     }
+  //   },
+  // },
 };
 
 export default projectsList;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -16,10 +16,12 @@ class Project extends Component {
         </header>
         <main className="page-main">
           <section className="project-demonstrations">
-            <div className="gif-mobile">
-              <h2>Mobile</h2>
-              <img src={gifs.mobile} alt="Mobile demonstration" />
-            </div>
+            {gifs.mobile ? (
+              <div className="gif-mobile">
+                <h2>Mobile</h2>
+                <img src={gifs.mobile} alt="Mobile demonstration" />
+              </div>
+            ) : null}
             <div className="gif-desktop">
               <h2>Desktop</h2>
               <img src={gifs.desktop} alt="Desktop demonstration" />
