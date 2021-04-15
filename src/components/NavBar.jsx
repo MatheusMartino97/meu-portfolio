72fd6f1766ce9c719526ca932354d8f96daec2f6
Matheus Martino <matheus.martino97@gmail.com>
2021-04-14 21:04:59 -0300

    Making the site responsible.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,14 +8,34 @@ class NavBar extends Component {
         <div className="nav-name">
           <h1>Matheus Martino do Amaral</h1>
         </div>
-      <div className="nav-social-links">
-        <a className="social-link" href="https://github.com/MatheusMartino97/" target="_blank" rel="noreferrer">
-          <img src="https://cdn.iconscout.com/icon/free/png-256/github-108-438008.png" width="48px" height="48px" alt="Github"/>
-        </a> 
-        <a className="social-link" href="https://www.linkedin.com/in/matheus-martino/" target="_blank" rel="noreferrer">
-          <img src="https://i.ibb.co/Kx2GSrT/linkedin.png" width="48px" height="48px" alt="Linked In"/>
-        </a>
-      </div>
+        <div className="nav-social-links">
+          <a
+            className="social-link"
+            href="https://github.com/MatheusMartino97/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/github-108-438008.png"
+              width="48px"
+              height="48px"
+              alt="Github"
+            />
+          </a>
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/matheus-martino/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://i.ibb.co/Kx2GSrT/linkedin.png"
+              width="48px"
+              height="48px"
+              alt="Linked In"
+            />
+          </a>
+        </div>
         <div className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/portfolio">Portfólio</Link>
@@ -24,9 +44,24 @@ class NavBar extends Component {
           <Link to="/contact">Contato</Link>
           <Link to="/about">Sobre</Link>
         </div>
+        <div className="dropdown">
+          <img
+            width="50px"
+            src="https://i.ibb.co/C2JTcgF/menu-button.png"
+            alt=""
+          />
+          <div className="dropdown-content">
+            <Link to="/">Home</Link>
+            <Link to="/portfolio">Portfólio</Link>
+            <Link to="/curriculum">Currículo</Link>
+            <Link to="/services">Tecnologias</Link>
+            <Link to="/contact">Contato</Link>
+            <Link to="/about">Sobre</Link>
+          </div>
+        </div>
       </nav>
     );
   }
 }
 
-export default NavBar
+export default NavBar;
