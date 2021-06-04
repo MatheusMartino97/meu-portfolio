71a8f234278aa46cea550a122858f179ebe8f6ef
Matheus Martino <matheus.martino97@gmail.com>
2021-06-03 23:04:04 -0300

    Adicionando pipe.

--- src/components/ChooseStack.jsx
+++ src/components/ChooseStack.jsx
@@ -1,20 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import projectsContext from '../context/projectsContext';
 import NavBar from './NavBar';
 
 function ChooseStack() {
   const { chooseTheStack } = useContext(projectsContext);
+  const [usePipeVisibility, setPipeVisibility] = useState('');
+
+  // setInterval(() => {
+  //   if (usePipeVisibility === 'hidden') {
+  //     setPipeVisibility('');
+  //   } else {
+  //     setPipeVisibility('hidden');
+  //   }
+  // }, 500);
 
   return (
     <div id="choose-page" className="page-content">
       <NavBar />
       <main className="choose-main">
         <section>
-          <h1>Escolha uma Stack</h1>
+          <h1>
+            Escolha uma Stack.{' '}
+            <span style={{ visibility: usePipeVisibility }}>|</span>
+          </h1>
 
           <div className="choose-links">
-            <button type="button" onClick={ () => { chooseTheStack('front') } }>Front-End</button>
-            <button type="button" onClick={ () => { chooseTheStack('back') } }>Back-End</button>
+            <button
+              type="button"
+              onClick={() => {
+                chooseTheStack('front');
+              }}
+            >
+              Front-End
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                chooseTheStack('back');
+              }}
+            >
+              Back-End
+            </button>
           </div>
         </section>
       </main>
